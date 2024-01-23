1f86c845cf3e178fb0b48f889ff130ac36ae211d
danielketterer <danielnorton0295@gmail.com>
2024-01-23 12:43:29 -0500

    profile page homework

---
+++ script.js
@@ -0,0 +1,9 @@
+function editProfile() {
+    var newName = prompt("Please enter your name");
+    return(newName);
+}
+
+function removeUser(){
+    var element = document.querySelector('.imgText');
+    element.remove();
+}
